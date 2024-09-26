23c4cc6ea2a04d4ab0f9b12a4223a6e7bf2cd212
bodelha <bodelha@gmail.com>
2024-09-25 21:48:32 -0300

    Simplifica

--- pizza/actions/actions.py
+++ pizza/actions/actions.py
@@ -1,27 +1,36 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 # from typing import Any, Text, Dict, List
-#
+
 # from rasa_sdk import Action, Tracker
 # from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
+
+
+# class ActionResponderCapital(Action):
+
 #     def name(self) -> Text:
-#         return "action_hello_world"
-#
+#         return "action_responder_capital"
+
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
+
+#         pais = tracker.get_slot("pais")
+
+#         capitais = {
+#             "brasil": "Brasília",
+#             "portugal" : "Lisboa",
+#             "argentina" : "Buenos Aires",
+#             "japão" : "Tokio",
+#             "alemanha" : "Berlim"
+#         }
+
+#         if pais:
+#             pais_lower = pais.lower()
+#             if pais_lower in capitais:
+#                 capital = capitais[pais_lower]
+#                 dispatcher.utter_message(text=f"A capital de {pais} é {capital}.")
+#             else:
+#                 dispatcher.utter_message(response="utter_capital_nao_sei")
+#         else:
+#             dispatcher.utter_message(text="Não entendi a pergunta, poderia repetir?")
+        
 #         return []
