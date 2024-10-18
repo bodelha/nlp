4db897a615c0cbc8fbd3cb38e8c6cea3b3a687d2
bodelha <bodelha@gmail.com>
2024-10-18 20:01:14 -0300

    Melhorias

--- github_api/actions/actions.py
+++ github_api/actions/actions.py
@@ -20,20 +20,21 @@ class ActionGetGithubRepositoriesForm(Action):
     ) -> List[Dict[Text, Any]]:
 
         github_username = tracker.get_slot('github_username')
+        linguagem = tracker.get_slot('linguagem')
 
         if not github_username:
             dispatcher.utter_message(
                 text="Nome de usuário do GitHub inválido. Tente novamente.")
             return [AllSlotsReset()]
         else:
-            github_repositories = await get_github_repositories(github_username)
+            github_repositories = await get_github_repositories(github_username, linguagem)
             dispatcher.utter_message(
                 response='utter_github_response', repositories=github_repositories)
             return [AllSlotsReset()]
               
 
 @lru_cache(maxsize=128)
-async def get_github_repositories(github_username: Text) -> Text:
+async def get_github_repositories(github_username: Text, linguagem: Text =None) -> Text:
     try:
         async with aiohttp.ClientSession() as session:
             url = f"https://api.github.com/users/{github_username}/repos"
@@ -44,29 +45,33 @@ async def get_github_repositories(github_username: Text) -> Text:
                         return f"Sem repositórios"
 
                     repo_messages = []
-                    for github_repositorie in github_repositories[:5]:
-                        repo_name = github_repositorie["name"] 
+                    for github_repositorie in github_repositories:
+                        repo_name = github_repositorie["name"]
                         languages_url = github_repositorie["languages_url"]
                         async with session.get(languages_url) as languages_response:
                             if languages_response.status == 200:
                                 languages = await languages_response.json()
                                 if languages:
                                     top_language = Counter(languages).most_common(1)[0][0]
-                                    repo_messages.append(f"- {repo_name} ({top_language})")
-                                else:
-                                    repo_messages.append(f"- {repo_name} (linguagem não detectada)")
+                                    if linguagem is None or top_language.lower() == linguagem.lower():
+                                        repo_messages.append(f"- {repo_name} ({top_language})")
                             else:
                                 repo_messages.append(f"- {repo_name} (erro ao obter linguagem)")
-                    if len(github_repositories) > 5:
-                        repo_messages.append("E mais outros repositórios")
+                    
+                    if repo_messages:
+                        return f"Repositórios de {github_username} com a linguagem {linguagem}:\n\n" + "\n".join(repo_messages)
+                    else:
+                        return f"Não há repositórios de {github_username} com a linguagem {linguagem}."
 
-                    return f"Alguns repositórios de {github_username}:\n\n" + "\n".join(repo_messages)
                 elif response.status == 403:
                     return f"Erro 403: Acesso à API do GitHub bloqueado. Tente novamente mais tarde. Pode ter atingido o limite de requisições."
+
                 elif response.status == 404:
                     return f"Desculpe, não encontrei o usuário {github_username} no GitHub."
+
                 else:
-                    return f"Erro ao acessar a API do GitHub."
+                    return f"Erro ao acessar a API do GitHub. Código de status: {response.status}"
+
     except aiohttp.ClientError as e:
         return f"Erro na requisição à API do GitHub: {e}"
     except json.JSONDecodeError as e:
